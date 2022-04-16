41f3039b44371456e97834b839f240b22cef7496
Kuro Okami <patrygaly01@gmail.com>
2022-04-16 14:33:29 -0300

    TP1

---
+++ TP1/Entidades/Operando.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        /// <summary>
+        /// Inicializa el Operando en 0.
+        /// </summary>
+        public Operando() : this(0)
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="numero"> Parametro que da valor al numero. </param>
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        /// <summary>
+        /// Inicializa el numero.
+        /// </summary>
+        /// <param name="setNumero"> Numero pasado en formato String. </param>
+        public Operando(string setNumero)
+        {
+            this.SetNumero = setNumero;
+        }
+
+        /// <summary>
+        /// Verifica que le valor pasado por parametro sea numerico.
+        /// </summary>
+        /// <param name="strNumero"> Parametro a validar. </param>
+        /// <returns> Retorna el valor del parametro en double, si no 0 en caso que no valide. </returns>
+        private double ValidarOperando(string strNumero)
+        {
+            if (double.TryParse(strNumero, out double retorno))
+            {
+                return retorno;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Setea el atributo numero, en caso que no valide 0.
+        /// </summary>
+        private string SetNumero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        /// <summary>
+        /// Verifica que el parametro sea binario.
+        /// </summary>
+        /// <param name="binario"> Parametro a validar. </param>
+        /// <returns> Retorna True si es binario, si no False si no es asi. </returns>
+        private static bool EsBinario(string binario)
+        {
+            bool retorno = true;
+
+            for (int i = 0; i < binario.Length; i++)
+            {
+                if (binario[i] < '0' || binario[i] > '1')
+                {
+                    retorno = false;
+                }
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Convierte el parametro a decimal.
+        /// </summary>
+        /// <param name="binario"> Parametro a convertir. </param>
+        /// <returns> Retorna un decimal (String), o "Valor Invalido" si no se puedo convertir. </returns>
+        public static string BinarioDecimal(string binario)
+        {
+            string retorno = "Valor Invalido";
+
+            if (EsBinario(binario))
+            {
+                retorno = Convert.ToInt32(binario, 2).ToString();
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Convierte el parametro a binario.
+        /// </summary>
+        /// <param name="numero"> Parametro a convertir. </param>
+        /// <returns> Retorna el numero en binario (String) </returns>
+        public static string DecimaBinario(double numero)
+        {
+            string binario = "";
+            int d = (int) numero;
+            int resto;
+            do
+            {
+                resto = d % 2;
+                if (resto == 0)
+                {
+                    //binario += resto;
+                    binario = binario.Insert(0, "0");
+                }
+                else
+                {
+                    //binario += "1";
+                    binario = binario.Insert(0, "1");
+                }
+                d = d / 2;
+            } while (d > 0);
+            return binario;
+        }
+
+        /// <summary>
+        /// Convierte el parametro a binario.
+        /// </summary>
+        /// <param name="numero"> Parametro a convertir. </param>
+        /// <returns> Retorna el numero en binario (String), o "Valor Invalido" si el numero no es un String. </returns>
+        public static string DecimaBinario(string numero)
+        {
+            string retorno = "Valor Invalido";
+            double aux;
+
+            if (double.TryParse(numero, out aux))
+            {
+                retorno = Operando.DecimaBinario(aux);
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Sobrecarga del operador "+".
+        /// </summary>
+        /// <param name="n1"> Primer objeto Operando </param>
+        /// <param name="n2"> Segundo objeto Operando </param>
+        /// <returns> Retorna la operacion entre el atributo numero. </returns>
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        /// <summary>
+        /// Sobrecarga del operador "-".
+        /// </summary>
+        /// <param name="n1"> Primer objeto Operando </param>
+        /// <param name="n2"> Segundo objeto Operando </param>
+        /// <returns> Retorna la operacion entre el atributo numero. </returns>
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        /// <summary>
+        /// Sobrecarga del operador "*".
+        /// </summary>
+        /// <param name="n1"> Primer objeto Operando </param>
+        /// <param name="n2"> Segundo objeto Operando </param>
+        /// <returns> Retorna la operacion entre el atributo numero. </returns>
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        /// <summary>
+        /// Sobrecarga del operador "/".
+        /// </summary>
+        /// <param name="n1"> Primer objeto Operando </param>
+        /// <param name="n2"> Segundo objeto Operando </param>
+        /// <returns> Retorna la operacion entre el atributo numero. </returns>
+        public static double operator /(Operando n1, Operando n2)
+        {
+            if (n2.numero == 0)
+            {
+                return double.MinValue;
+            }
+            else
+            {
+                return n1.numero / n2.numero;
+            }
+        }
+    }
+}
---
+++ TP1/TP1-1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Entidades;
+
+namespace TP1_1
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Inicializa las operaciones e impide la edicion por teclado en combobox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            cmbOperador.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOperador.Items.Add("+");
+            cmbOperador.Items.Add("-");
+            cmbOperador.Items.Add("/");
+            cmbOperador.Items.Add("*");
+        }
+
+        /// <summary>
+        /// Opera con dos numeros (string) con un operador (string).
+        /// </summary>
+        /// <param name="numero1"> Primer numero a operar (string) </param>
+        /// <param name="numero2"> Segundo numero a operar (string) </param>
+        /// <param name="operador"> Operador (string) </param>
+        /// <returns></returns>
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Operando num1 = new Operando(numero1);
+            Operando num2 = new Operando(numero2);
+
+            return Calculadora.Operar(num1, num2, operador);
+        }
+
+        /// <summary>
+        /// Limpia el TextBox, ComboBox, Labels.
+        /// </summary>
+        private void Limpiar()
+        {
+            this.txtNumero1.Text = string.Empty;
+            this.txtNumero2.Text = string.Empty;
+            this.cmbOperador.Text = string.Empty;
+            this.lblResultado.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Evento del boton Operar.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado = FormCalculadora.Operar(this.txtNumero1.Text, this.txtNumero2.Text, this.cmbOperador.Text);
+            this.lblResultado.Text = resultado.ToString();
+            lstOperaciones.Items.Add($"{txtNumero1.Text} {this.cmbOperador.Text} {txtNumero2.Text} = {resultado.ToString()}");
+        }
+
+        /// <summary>
+        /// Envento del boton Limpiar.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        /// <summary>
+        /// Evento del boton Cerrar.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Evento del boton Convertir a Binario.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Operando.DecimaBinario(this.lblResultado.Text);
+        }
+
+        /// <summary>
+        /// Evento del boton Decimal a Binario.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Operando.BinarioDecimal(this.lblResultado.Text);
+        }
+
+        /// <summary>
+        /// Pregunta si desea salir del Form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que quiere salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
